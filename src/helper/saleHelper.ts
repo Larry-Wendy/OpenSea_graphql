38b55305328b421772d9bdfa3cf5d9d74fdcbbde
Shi Jiarong <js6132@columbia.edu>
2023-05-01 17:22:22 -0400

    Add files via upload

---
+++ src/helper/saleHelper.ts
@@ -0,0 +1,34 @@
+import { BigDecimal, Bytes, ethereum } from '@graphprotocol/graph-ts'
+import { User, Collection, Sale } from '../../generated/schema'
+import GlobalConstants from '../utils'
+
+export function getOrCreateSale(event: ethereum.Event): Sale {
+	let sale = Sale.load(GlobalConstants.globalId(event))
+	if (!sale) {
+		sale = new Sale(GlobalConstants.globalId(event))
+		sale.timestamp = event.block.timestamp
+		sale.txHash = event.transaction.hash
+		sale.blockHash = event.block.hash
+		sale.logNumber = event.logIndex
+		sale.blockNumber = event.block.number
+		sale.eventType = 'SALE'
+	}
+	return sale as Sale
+}
+
+export function updateSale(
+	sale: Sale,
+	buyHash: Bytes,
+	sellHash: Bytes,
+	buyer: User,
+	seller: User,
+	price: BigDecimal,
+	collection: Collection
+): void {
+	sale.buyHash = buyHash
+	sale.sellHash = sellHash
+	sale.seller = seller.id
+	sale.buyer = buyer.id
+	sale.price = price
+	sale.collection = collection.id
+}
